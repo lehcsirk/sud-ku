66ef7f2e9c7e2cc30e0cf36479078a1a9e2bd262
Cameron Krischel <ckrischel@he3labs.com>
2019-02-15 13:18:22 -0800

    Add files via upload
    Empty project atm just committing

---
+++ sudōku/sudōku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sudōku
+//
+//  Created by Cameron Krischel on 2/15/19.
+//  Copyright © 2019 Cameron Krischel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
